17c41f1f4b59fb3579271b8b6f769a7c9b70a3a5
Azran Hossain <mr69751@gmail.com>
2023-08-15 11:17:18 +0600

    [mobile] login page

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,52 @@
+import Link from "next/link";
+
+export default function page() {
+    return (
+        <>
+            <main className="flex flex-col justify-center items-center h-screen">
+
+                <section className="text-center mb-10">
+                    <span className="text-xl">Sign in to</span> <br />
+                    <h1 className="text-3xl font-bold text-white">Esports FTW</h1>
+                </section>
+
+                <form className="w-80">
+                    <div className="form-control w-full">
+                        <label className="label">
+                            <span className="label-text">Email</span>
+                        </label>
+                        <input type="email" placeholder="someone@mail.com" className="input input-bordered w-full" />
+                    </div>
+                    <div className="form-control w-full">
+                        <label className="label">
+                            <span className="label-text">Password</span>
+                        </label>
+                        <input type="password" placeholder="***********" className="input input-bordered w-full" />
+                    </div>
+                    <div className="flex justify-between items-end">
+                        <div className="mt-5">
+                            <label className="flex gap-4 items-center">
+                                <input type="checkbox" className="checkbox checkbox-primary" />
+                                <span className="label-text ">Remember me</span>
+                            </label>
+                        </div>
+                        <div className="underline label-text">
+                            <Link href='/forgot' > Forgot password?</Link>
+                        </div>
+                    </div>
+                    <button className=" w-full bg-white py-2 mt-8 rounded-lg text-black">
+                        Sign in
+                    </button>
+                </form>
+
+                <div className="mt-5">
+                    Don't have an account yet ?
+                    <span className="underline label-text">
+                        <Link href='/registration' > Sign up </Link>
+                    </span>
+                </div>
+
+            </main>
+        </>
+    )
+}
