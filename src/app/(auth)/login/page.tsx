1a5d6dbf5d6298d3cefe0de7bae57d90335d9e4f
Azran Hossain <mr69751@gmail.com>
2023-08-15 11:35:06 +0600

    responsive desktop version

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -5,14 +5,14 @@ export default function page() {
     return (
         <>
             <Image src="/esports-ftw.png" alt="bg" width={150} height={150} className="m-auto mt-10" />
-            <main className="flex flex-col justify-center items-center h-screen">
+            <main className="flex flex-col justify-center items-center h-screen ">
 
                 <section className="text-center mb-10">
                     <span className="text-xl">Sign in to</span> <br />
                     <h1 className="text-3xl font-bold text-white">Esports FTW</h1>
                 </section>
 
-                <form className="w-80">
+                <form className="w-80 md:w-full md:px-10 lg:w-[800px] ">
                     <div className="form-control w-full">
                         <label className="label">
                             <span className="label-text">Email</span>
