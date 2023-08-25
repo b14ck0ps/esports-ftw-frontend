66ce948eb07fe970d8464305c0469c8b091f69dc
Azran Hossain <mr69751@gmail.com>
2023-08-25 15:51:49 +0600

    player auth +

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,7 +1,35 @@
+'use client'
 import Link from "next/link";
 import Image from "next/image";
+import axios from "axios";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function page() {
+
+    const router = useRouter()
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [loginError, setLoginError] = useState('')
+
+    async function handleLogin(e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault()
+
+        try {
+            const res = await axios.post('https://localhost:7033/api/Auth', { email, password })
+            if (res.status !== 200) {
+                setLoginError(res.data)
+                return
+            } else {
+                router.push('/dashboard')
+            }
+        } catch (error) {
+            setLoginError('Invalid email or password')
+            return
+        }
+    }
+
     return (
         <>
             <main className="flex flex-col justify-center items-center h-screen ">
@@ -13,18 +41,27 @@ export default function page() {
                     </Link>
                 </section>
 
-                <form className="w-80 md:w-full md:px-10 lg:w-[800px] ">
+                <form onSubmit={handleLogin} className="w-80 md:w-full md:px-10 lg:w-[800px] ">
+
+                    {loginError && <div className="alert alert-error">{loginError}</div>}
+
                     <div className="form-control w-full">
                         <label className="label">
                             <span className="label-text">Email</span>
                         </label>
-                        <input type="email" placeholder="someone@mail.com" className="input input-bordered w-full" />
+                        <input
+                            value={email}
+                            onChange={e => setEmail(e.target.value)}
+                            type="email" placeholder="someone@mail.com" className="input input-bordered w-full" />
                     </div>
                     <div className="form-control w-full">
                         <label className="label">
                             <span className="label-text">Password</span>
                         </label>
-                        <input type="password" placeholder="***********" className="input input-bordered w-full" />
+                        <input
+                            value={password}
+                            onChange={e => setPassword(e.target.value)}
+                            type="password" placeholder="***********" className="input input-bordered w-full" />
                     </div>
                     <div className="flex justify-between items-end">
                         <div className="mt-5">
@@ -37,7 +74,7 @@ export default function page() {
                             <Link href='/forgot' > Forgot password?</Link>
                         </div>
                     </div>
-                    <button className=" w-full bg-white py-2 mt-8 rounded-lg text-black">
+                    <button type="submit" className=" w-full bg-white py-2 mt-8 rounded-lg text-black">
                         Sign in
                     </button>
                 </form>
